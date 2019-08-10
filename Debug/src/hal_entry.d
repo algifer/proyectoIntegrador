15234e7e67a35747f677df9f38041197e5d10ce1
Algemiro Gil Fernandez <agilf@emdep.local>
2019-08-10 11:43:44 -0500

    Interrupciones
    Se añade ejercicio de interrupciones

---
+++ Debug/src/hal_entry.d
@@ -0,0 +1,172 @@
+src/hal_entry.o: ../src/hal_entry.c \
+ C:\repos\Interrupciones\src\synergy_gen/hal_data.h \
+ C:\repos\Interrupciones\synergy\ssp\inc\bsp/bsp_api.h \
+ c:\repos\interrupciones\synergy\ssp\inc\ssp_common_api.h \
+ c:\repos\interrupciones\synergy\ssp\inc\ssp_version.h \
+ C:\repos\Interrupciones\synergy_cfg\ssp_cfg\bsp/bsp_cfg.h \
+ C:\repos\Interrupciones\synergy_cfg\ssp_cfg\bsp/bsp_clock_cfg.h \
+ C:\repos\Interrupciones\synergy_cfg\ssp_cfg\bsp/bsp_mcu_family_cfg.h \
+ C:\repos\Interrupciones\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_pn_cfg.h \
+ C:\repos\Interrupciones\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_cfg.h \
+ c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\s7g2\bsp_mcu_info.h \
+ c:\repos\interrupciones\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\s7g2.h \
+ C:\repos\Interrupciones\synergy\ssp\inc\bsp\cmsis\Include/core_cm4.h \
+ C:\repos\Interrupciones\synergy\ssp\inc\bsp\cmsis\Include/cmsis_version.h \
+ C:\repos\Interrupciones\synergy\ssp\inc\bsp\cmsis\Include/cmsis_compiler.h \
+ C:\repos\Interrupciones\synergy\ssp\inc\bsp\cmsis\Include/cmsis_gcc.h \
+ C:\repos\Interrupciones\synergy\ssp\inc\bsp\cmsis\Include/mpu_armv7.h \
+ c:\repos\interrupciones\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\system_S7G2.h \
+ c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\s7g2\bsp_elc.h \
+ c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\s7g2\bsp_cache.h \
+ c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\s7g2\bsp_analog.h \
+ c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_common_analog.h \
+ C:\repos\Interrupciones\synergy_cfg\ssp_cfg\bsp/bsp_board_cfg.h \
+ c:\repos\interrupciones\synergy\board\s7g2_sk\bsp.h \
+ c:\repos\interrupciones\synergy\board\s7g2_sk\bsp_init.h \
+ c:\repos\interrupciones\synergy\board\s7g2_sk\bsp_leds.h \
+ c:\repos\interrupciones\synergy\board\s7g2_sk\bsp_qspi.h \
+ c:\repos\interrupciones\synergy\board\s7g2_sk\bsp_ethernet_phy.h \
+ c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_common.h \
+ c:\repos\interrupciones\synergy\ssp\inc\ssp_common_api.h \
+ c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h \
+ c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_register_protection.h \
+ c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_locking.h \
+ c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_irq.h \
+ c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_group_irq.h \
+ c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_clocks.h \
+ C:\repos\Interrupciones\synergy\ssp\inc\driver\api/r_ioport_api.h \
+ c:\repos\interrupciones\synergy\ssp\inc\ssp_features.h \
+ c:\repos\interrupciones\synergy\ssp\inc\ssp_common_api.h \
+ c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h \
+ C:\repos\Interrupciones\synergy\ssp\inc\driver\instances/r_fmi.h \
+ C:\repos\Interrupciones\synergy\ssp\inc\driver\api/r_fmi_api.h \
+ c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_common_leds.h \
+ c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_delay.h \
+ c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_feature.h \
+ c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_mcu_api.h \
+ c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_error_checking.h \
+ C:\repos\Interrupciones\src\synergy_gen/common_data.h \
+ C:\repos\Interrupciones\synergy\ssp\inc\driver\instances/r_ioport.h \
+ C:\repos\Interrupciones\synergy_cfg\ssp_cfg\driver/r_ioport_cfg.h \
+ C:\repos\Interrupciones\synergy\ssp\inc\driver\instances/r_cgc.h \
+ C:\repos\Interrupciones\synergy_cfg\ssp_cfg\driver/r_cgc_cfg.h \
+ C:\repos\Interrupciones\synergy\ssp\inc\driver\api/r_cgc_api.h \
+ C:\repos\Interrupciones\synergy\ssp\inc\driver\instances/r_elc.h \
+ C:\repos\Interrupciones\synergy_cfg\ssp_cfg\driver/r_elc_cfg.h \
+ C:\repos\Interrupciones\synergy\ssp\inc\driver\api/r_elc_api.h \
+ C:\repos\Interrupciones\synergy\ssp\inc\driver\instances/r_icu.h \
+ C:\repos\Interrupciones\synergy\ssp\inc\driver\api/r_external_irq_api.h
+
+C:\repos\Interrupciones\src\synergy_gen/hal_data.h:
+
+C:\repos\Interrupciones\synergy\ssp\inc\bsp/bsp_api.h:
+
+c:\repos\interrupciones\synergy\ssp\inc\ssp_common_api.h:
+
+c:\repos\interrupciones\synergy\ssp\inc\ssp_version.h:
+
+C:\repos\Interrupciones\synergy_cfg\ssp_cfg\bsp/bsp_cfg.h:
+
+C:\repos\Interrupciones\synergy_cfg\ssp_cfg\bsp/bsp_clock_cfg.h:
+
+C:\repos\Interrupciones\synergy_cfg\ssp_cfg\bsp/bsp_mcu_family_cfg.h:
+
+C:\repos\Interrupciones\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_pn_cfg.h:
+
+C:\repos\Interrupciones\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_cfg.h:
+
+c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\s7g2\bsp_mcu_info.h:
+
+c:\repos\interrupciones\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\s7g2.h:
+
+C:\repos\Interrupciones\synergy\ssp\inc\bsp\cmsis\Include/core_cm4.h:
+
+C:\repos\Interrupciones\synergy\ssp\inc\bsp\cmsis\Include/cmsis_version.h:
+
+C:\repos\Interrupciones\synergy\ssp\inc\bsp\cmsis\Include/cmsis_compiler.h:
+
+C:\repos\Interrupciones\synergy\ssp\inc\bsp\cmsis\Include/cmsis_gcc.h:
+
+C:\repos\Interrupciones\synergy\ssp\inc\bsp\cmsis\Include/mpu_armv7.h:
+
+c:\repos\interrupciones\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\system_S7G2.h:
+
+c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\s7g2\bsp_elc.h:
+
+c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\s7g2\bsp_cache.h:
+
+c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\s7g2\bsp_analog.h:
+
+c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_common_analog.h:
+
+C:\repos\Interrupciones\synergy_cfg\ssp_cfg\bsp/bsp_board_cfg.h:
+
+c:\repos\interrupciones\synergy\board\s7g2_sk\bsp.h:
+
+c:\repos\interrupciones\synergy\board\s7g2_sk\bsp_init.h:
+
+c:\repos\interrupciones\synergy\board\s7g2_sk\bsp_leds.h:
+
+c:\repos\interrupciones\synergy\board\s7g2_sk\bsp_qspi.h:
+
+c:\repos\interrupciones\synergy\board\s7g2_sk\bsp_ethernet_phy.h:
+
+c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_common.h:
+
+c:\repos\interrupciones\synergy\ssp\inc\ssp_common_api.h:
+
+c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h:
+
+c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_register_protection.h:
+
+c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_locking.h:
+
+c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_irq.h:
+
+c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_group_irq.h:
+
+c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_clocks.h:
+
+C:\repos\Interrupciones\synergy\ssp\inc\driver\api/r_ioport_api.h:
+
+c:\repos\interrupciones\synergy\ssp\inc\ssp_features.h:
+
+c:\repos\interrupciones\synergy\ssp\inc\ssp_common_api.h:
+
+c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h:
+
+C:\repos\Interrupciones\synergy\ssp\inc\driver\instances/r_fmi.h:
+
+C:\repos\Interrupciones\synergy\ssp\inc\driver\api/r_fmi_api.h:
+
+c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_common_leds.h:
+
+c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_delay.h:
+
+c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_feature.h:
+
+c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_mcu_api.h:
+
+c:\repos\interrupciones\synergy\ssp\src\bsp\mcu\all\bsp_error_checking.h:
+
+C:\repos\Interrupciones\src\synergy_gen/common_data.h:
+
+C:\repos\Interrupciones\synergy\ssp\inc\driver\instances/r_ioport.h:
+
+C:\repos\Interrupciones\synergy_cfg\ssp_cfg\driver/r_ioport_cfg.h:
+
+C:\repos\Interrupciones\synergy\ssp\inc\driver\instances/r_cgc.h:
+
+C:\repos\Interrupciones\synergy_cfg\ssp_cfg\driver/r_cgc_cfg.h:
+
+C:\repos\Interrupciones\synergy\ssp\inc\driver\api/r_cgc_api.h:
+
+C:\repos\Interrupciones\synergy\ssp\inc\driver\instances/r_elc.h:
+
+C:\repos\Interrupciones\synergy_cfg\ssp_cfg\driver/r_elc_cfg.h:
+
+C:\repos\Interrupciones\synergy\ssp\inc\driver\api/r_elc_api.h:
+
+C:\repos\Interrupciones\synergy\ssp\inc\driver\instances/r_icu.h:
+
+C:\repos\Interrupciones\synergy\ssp\inc\driver\api/r_external_irq_api.h:
